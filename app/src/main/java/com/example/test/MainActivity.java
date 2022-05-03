281580b6bac890246af043cef0d9ae2a6aa6ae34
iitaellu <iitaellu@hotmail.com>
2022-05-03 16:56:47 +0300

    part 1 task. Two edit text for numbers, button to calculate two number together and textView to show result.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firtNumEditText = (EditText) findViewById(R.id.firtNumEditText);
+                EditText secondNumberEditText = (EditText) findViewById(R.id.secondNumberEditText);
+
+                TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
+
+                int num1 = Integer.parseInt(firtNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumberEditText.getText().toString());
+                int result = num1 + num2;
+                sumTextView.setText((result + ""));
+            }
+        });
     }
+
+
 }
